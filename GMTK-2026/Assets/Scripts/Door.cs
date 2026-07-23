eaa9913cc188903e2ac0da2a7a86902f79ac9654
RealWigglyOrange <realwigglyorange@gmail.com>
2026-07-23 11:56:58 -0400

    change interact key

--- GMTK-2026/Assets/Scripts/Door.cs
+++ GMTK-2026/Assets/Scripts/Door.cs
@@ -16,7 +16,7 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && interactable && buttonsNeeded == 0)
+        if (Input.GetKeyDown(KeyCode.E) && interactable && buttonsNeeded == 0)
         {
             openDoor();
         }
