f549af5966b479c977c02167f2d4b21910454aec
Ethan-Mulwee <167151595+Ethan-Mulwee@users.noreply.github.com>
2026-07-26 05:47:11 -0400

    big room

--- GMTK-2026/Assets/Scripts/Collect2.cs
+++ GMTK-2026/Assets/Scripts/Collect2.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Collect2 : MonoBehaviour
 {
+    public DoorTrigger doorTrigger;
 
     public WizardController wizard;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,6 +21,7 @@ public class Collect2 : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         wizard.spell2Enabled = true;
+        doorTrigger.unlock();
         Destroy(gameObject);
     }
 }
